c27fd1f9c408ad2a601d0bf81d12ae32b0ecb7b4
sahil bhatt <sahil bhatt@DESKTOP-QRMVD04>
2020-08-27 11:24:08 +0530

    sahilbhatt_firstproject

---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module sahilbhatt {
+}
---
+++ src/sahilbhatt/firstproject.java
@@ -0,0 +1,10 @@
+package sahilbhatt;
+
+public class firstproject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          System.out.println("HELLO WORLD");
+	}
+
+}
